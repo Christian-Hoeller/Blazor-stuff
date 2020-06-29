5979e6e567cd4dec2e0a62da22393f86eeb76cb2
Christian Höller <ch.hoeller14@gmail.com>
2020-06-29 18:54:18 +0200

    added WordStyle class

---
+++ Portfolio/Portfolio/Classes/WordStyle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portfolio.Classes
+{
+    public class WordStyle
+    {
+        public string BackgroundColor{ get; set; }
+        public string Color{ get; set; }
+        public override string ToString()
+        {
+            return $"color:{Color}; backgroung-color:{BackgroundColor};";
+        }
+    }
+}
