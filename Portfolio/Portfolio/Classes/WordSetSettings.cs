e7e41cb2c5dc9cb56d68456f37ab3b22e1d3fb6f
Christian Höller <ch.hoeller14@gmail.com>
2020-06-30 20:20:31 +0200

    added select option for wordset and removed timer

---
+++ Portfolio/Portfolio/Classes/WordSetSettings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portfolio.Classes
+{
+    public class WordSetSettings
+    {
+        public WordSetSettings(string language, int wordAmount)
+        {
+            Language = language;
+            WordAmount = wordAmount;
+        }
+
+        public string Language { get; set; }
+        public int WordAmount { get; set; }
+    }
+}
